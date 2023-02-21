ebf629bf7af545c13e2e293d498d74a2cbfd8687
YUTARO KONDA <kondayutarou@gmail.com>
2023-02-21 15:24:49 +0900

    Add theme function

--- lib/view/HomeView.dart
+++ lib/view/HomeView.dart
@@ -1,9 +1,19 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:stop_watch_timer/stop_watch_timer.dart';
+import 'package:provider/provider.dart';
 import 'package:stopwatch/StopWatchUtil.dart';
 
+class StopWatchTheme extends ChangeNotifier {
+  ThemeData current = ThemeData.light();
+  bool _isDark = false;
+
+  void toggle() {
+    _isDark = !_isDark;
+    current = _isDark ? ThemeData.dark() : ThemeData.light();
+    notifyListeners();
+  }
+}
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -35,34 +45,45 @@ class MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          children: [
-            FilledButton(onPressed: () {
-              _stopWatchTimer.start();
-            }, child: Text("Start")),
-            FilledButton(onPressed: () {
-              _stopWatchTimer.stop();
-            }, child: Text("Stop")),
-            FilledButton(onPressed: () {
-              _stopWatchTimer.reset();
-            }, child: Text("Reset")),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return ChangeNotifierProvider(create: (_) => StopWatchTheme(),
+    child: Consumer<StopWatchTheme>(
+      builder: (context, theme, _) {
+        return MaterialApp(
+          theme: theme.current,
+          home: Scaffold(
+            appBar: AppBar(),
+            body: Column(
               children: [
-                Text(_stopWatchTimer.elapsed.hoursForStopWatch().toString()),
-                Text(":"),
-                Text(_stopWatchTimer.elapsed.minutesForStopWatch().toString()),
-                Text(":"),
-                Text(_stopWatchTimer.elapsed.secondsForStopWatch().toString()),
+                FilledButton(onPressed: () {
+                  _stopWatchTimer.start();
+                }, child: Text("Start")),
+                FilledButton(onPressed: () {
+                  _stopWatchTimer.stop();
+                }, child: Text("Stop")),
+                FilledButton(onPressed: () {
+                  _stopWatchTimer.reset();
+                }, child: Text("Reset")),
+                FilledButton(onPressed: () {
+                  Provider.of<StopWatchTheme>(context, listen: false).toggle();
+                }, child: Text("Change theme")),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(_stopWatchTimer.elapsed.hoursForStopWatch().toString()),
+                    Text(":"),
+                    Text(_stopWatchTimer.elapsed.minutesForStopWatch().toString()),
+                    Text(":"),
+                    Text(_stopWatchTimer.elapsed.secondsForStopWatch().toString()),
+                  ],
+                )
               ],
-            )
-          ],
-        ),
-      ),
+            ),
+          ),
+        );
+      },
+    )
     );
+
+
   }
 }
